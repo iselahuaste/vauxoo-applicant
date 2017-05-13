a47045696f76b2c05650a6a4973d168d4b4f30d3
Isela <iselahuaste@gmail.com>
2017-05-12 23:26:35 -0500

    py2

--- calculator.py
+++ calculator.py
@@ -1,13 +1,12 @@
 """clase que suma los elementos de una lista"""
 
-class calculator_class:
-    def sum(lista):
-        """metodo que recibe como argumento una lista"""
-        suma=0
-        for i in lista:
-            suma=i+suma
+class calculator:
+
+    """metodo que recibe como argumento una lista"""
+    def sum(self,num_lista):
+
+        suma = 0
+        for i in num_lista:
+            suma = i + suma
         return suma
         """Devuelve el resultado de la suma de los elementos de la lista"""
-
-lista=[1,3,4,5]
-print sum(lista)
